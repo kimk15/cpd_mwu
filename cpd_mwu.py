4e5b27d12e7d3d50d39368ff1a63a2236dc58694
unknown <kkim154@illinois.edu>
2020-03-09 14:14:18 -0700

    Updated main

--- cpd_mwu.py
+++ cpd_mwu.py
@@ -14,42 +14,41 @@ Computes CPD for 3 dimensional tensors.
 Returns A,B,C
 """
 def CPD_MWU(X, F, sketching_rates, lamb, eps, nu, rank, num_iterations=100):
-	# Keep residual errors + res time
-	error = []
-	res_time = 0
+    # Keep residual errors + res time
+    error = []
+    res_time = 0
 
-	# Cache norm + Id + unfolding of X
-	norm_x = norm(X)
-	Id = np.eye(rank)
-	X_unfold = [unfold(X, mode=0), unfold(X, mode=1), unfold(X, mode=2)]
+    # Cache norm + Id + unfolding of X
+    norm_x = norm(X)
+    Id = np.eye(rank)
+    X_unfold = [unfold(X, mode=0), unfold(X, mode=1), unfold(X, mode=2)]
 
-	# Initialize weights
-	weights = np.array([1] * len(sketching_rates)) / (len(sketching_rates))
+    # Initialize weights
+    weights = np.array([1] * len(sketching_rates)) / (len(sketching_rates))
 
-	# Randomly initialize A,B,C
-	dim_1, dim_2, dim_3 = X.shape
-	A, B, C = rand_init(dim_1, rank), rand_init(dim_2, rank), rand_init(dim_3, rank)
+    # Randomly initialize A,B,C
+    dim_1, dim_2, dim_3 = X.shape
+    A, B, C = rand_init(dim_1, rank), rand_init(dim_2, rank), rand_init(dim_3, rank)
 
-	# Append initialization residual error
-	error.append(residual_error(X_unfold[0], norm_x, A,B,C))
+    # Append initialization residual error
+    error.append(residual_error(X_unfold[0], norm_x, A,B,C))
 
-	# Run CPD_MWU for num iterations
-	for i in range(num_iterations):
-		if (i+1)%5000 == 0:
-			print("iteration:", i)
-			start = time.time()
-			error.append(residual_error(X_unfold[0], norm_x, A,B,C))
-			end = time.time()
-			res_time += end-start
-		# Select sketching rate with probability proportional to w_i
-		s = sample(sketching_rates, weights)
+    # Run CPD_MWU for num iterations
+    for i in range(num_iterations):
+        # Select sketching rate with probability proportional to w_i
+        s = sample(sketching_rates, weights)
 
-		# Solve Ridge Regression for A,B,C
-		A, B, C = update_factors(A, B, C, X_unfold, Id, lamb, s, rank)
+        # Solve Ridge Regression for A,B,C
+        A, B, C = update_factors(A, B, C, X_unfold, Id, lamb, s, rank)
 
-		# Update weights
-		if bern(eps) == 1 and len(sketching_rates) > 1:
-			update_weights(A, B, C, X_unfold, Id, norm_x, lamb, weights, sketching_rates, rank, nu, eps)
-	
+        # Update weights
+        if bern(eps) == 1 and len(sketching_rates) > 1:
+            update_weights(A, B, C, X_unfold, Id, norm_x, lamb, weights, sketching_rates, rank, nu, eps)
+
+        print("iteration:", i)
+        start = time.time()
+        error.append(residual_error(X_unfold[0], norm_x, A,B,C))
+        end = time.time()
+        res_time += end-start
 	return A,B,C, np.array(error), res_time
 
--- main.py
+++ main.py
@@ -6,59 +6,31 @@ import time
 from cpd_mwu import CPD_MWU
 
 # Set up
+shape = (300, 300, 300)
 lamb = 0.001
 eps = 0.05 # 1/N where N = # interations
 nu = 2
 rank = 5  
-num_iterations = 100
-
-# Run experiment for no sketching
-no_sketch_error = np.array([0.0] * 101)
-sketching_rates = [1]
-start = time.time()
-for i in range(5):
-    X = tl_rand.random_kruskal((366,366,100), 5, full=True)
-    A,B,C, error = CPD_MWU(X, sketching_rates, lamb, eps, nu, rank, True, num_iterations)
-    no_sketch_error += error
-# Print out total time
-print("Total time for 5 iterations:", time.time()-start)
-# Average out error
-no_sketch_error /= 5
-
-# Run experiment for sketching without weight update
-sketching_rates = list(np.linspace(10**(-3), 10**(-1), 4)) + [1]
-sketch_no_weight_update_error = np.array([0.0] * 101)
-start = time.time()
-for i in range(5):
-    X = tl_rand.random_kruskal((366,366,100), 5, full=True)
-    A,B,C, error = CPD_MWU(X, sketching_rates, lamb, eps, nu, rank, False, num_iterations)
-    sketch_no_weight_update_error += error
-# Print out total time
-print("Total time for 5 iterations:", time.time()-start)
-# Average out error
-sketch_no_weight_update_error /= 5
+num_iterations = 1000
+F = np.array(tl_rand.random_kruskal(shape=shape, rank=rank, full=False, random_state=np.random.RandomState(seed=0)))
+X = tl_kruskal.kruskal_to_tensor(F)
 
 # Run experiment for sketching with weight update
 sketching_rates = list(np.linspace(10**(-3), 10**(-1), 4)) + [1]
-sketch_with_weight_update_error = np.array([0.0] * 101)
 start = time.time()
-for i in range(5):
-    X = tl_rand.random_kruskal((366,366,100), 5, full=True)
-    A,B,C, error = CPD_MWU(X, sketching_rates, lamb, eps, nu, rank, True, num_iterations)
-    sketch_with_weight_update_error += error
+A,B,C, error, res_time = CPD_MWU(X, F, sketching_rates, lamb, eps, nu, rank, num_iterations)
+end = time.time()
 # Print out total time
-print("Total time for 5 iterations:", time.time()-start)
-# Average out error
-sketch_with_weight_update_error /= 5
+print("Total time", end-start)
+print("CPD time", end-start-res_time)
+print("Residual error time", res_time)
+
 
 
 # Plot out error
-x = [i for i in range(len(error))]
+x = range(len(error))
 plt.title('Residual error vs # iterations')
 plt.xlabel('Iterations')
 plt.ylabel('Residual Error')
-plt.plot(x, no_sketch_error, label='No sketch + weight update')
-plt.plot(x, sketch_no_weight_update_error, label='Sketch + w/o weight update')
-plt.plot(x, sketch_with_weight_update_error, label='Sketch + weight update')
-plt.legend(loc="upper left")
+plt.plot(x, error, label='No sketch + weight update')
 plt.figure()
